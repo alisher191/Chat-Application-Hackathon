c8a26093de1e8f0536b7615248d4dd9d74257153
emir <e352709@gmail.com>
2023-04-02 03:55:03 +0600

    commit

--- ChatApp/ChatApp/settings.py
+++ ChatApp/ChatApp/settings.py
@@ -10,10 +10,10 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 import os
-
 from pathlib import Path
 
-# Build paths inside the project like this: BASE_DIR / 'subdir'.
+
+# Build paths inside the project like this: BASE_DIR / 'subdi'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     # apps
     'chat.apps.ChatConfig',
     'rest_framework',
+    'account.apps.AccountConfig',
     # 'widget_tweaks',
 ]
 
@@ -80,8 +81,12 @@ WSGI_APPLICATION = 'ChatApp.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'chatapp',
+        'USER': 'emir',
+        'PASSWORD': '1',
+        'HOST': 'localhost',
+        'PORT': 5432
     }
 }
 
